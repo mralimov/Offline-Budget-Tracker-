6ce51b21660d09c5cb0e5f87c913f05e7fc3d5e2
mralimov <mralimov89@gmail.com>
2020-10-26 11:17:05 -0400

    updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 
 const app = express();
 
@@ -17,9 +17,6 @@ app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 // routes
